d3b593189862fbea6a04ee8b5611c71348709825
Rakesh R <rakeshr1k9@gmail.com>
2022-02-21 19:34:50 +0530

    backup git

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openSideNav() {
+  document.getElementById("side-nav-bar-section").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeSideNav() {
+  document.getElementById("side-nav-bar-section").style.width = "0";
+}
